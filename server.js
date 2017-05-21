b68f96855d92de4c69bee2f9652a940a2033e89d
msolorio <msolorio789@gmail.com>
2017-05-20 19:29:36 -0700

    experiment with custom middleware functions

--- server.js
+++ server.js
@@ -4,9 +4,54 @@ const express = require('express');
 // instantiate our express app
 const app = express();
 
+////////////////////////////////////////////////////////////////////
+// CUSTOM MIDDLEWARE FUNCTIONS
+////////////////////////////////////////////////////////////////////
+function requestLogger(request, response, next) {
+  const now = new Date();
+
+  console.log(`/////////////////////////////////////////////////////////////////\nRequest Information:\n${now.toLocaleDateString()}\n${now.toLocaleTimeString()}\n${request.method}\n${request.url}\n/////////////////////////////////////////////////////////////////`);
+
+  next();
+}
+
+function checkQueryParams(request, response, next) {
+  if (request.query.notAMember === 'true') {
+    return response.json({notAMember: true});
+  }
+  next();
+}
+
+function urlBMiddleware1(request, response, next) {
+  console.log(`urlBMiddleware1`);
+  next();
+}
+
+function urlBMiddleware2(request, response, next) {
+  console.log(`urlBMiddleware2`);
+  next();
+}
+
+function urlBMiddleware3(request, response, next) {
+  console.log(`urlBMiddleware3`);
+  next();
+}
+
+const urlBMiddlewares = [urlBMiddleware1, urlBMiddleware2, urlBMiddleware3];
+
+app.use(requestLogger);
+
+app.use(checkQueryParams);
+
 app.get('/', (request, response) => {
   response.sendFile(`${__dirname}/views/index.html`);
+  console.log(request.hostname);
 });
+
+app.get('/urlA', (request, response) => { response.send('response for urlA'); });
+
+app.get('/urlB', urlBMiddlewares, (request, response) => { response.send('response for urlB'); });
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`listening on port ${process.env.PORT || 8080}`);
